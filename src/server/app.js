c95b7127b8cd703871e96282a18ddf6a56f2e6f9
brayanmm6 <martinsbrayan514@gmail.com>
2025-01-16 20:42:54 -0300

    Resolvendo controle de acesso

--- src/server/app.js
+++ src/server/app.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 app.get("/show", async (req, res) => {
     const client = await db.show()
-    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500, http://localhost:5173")
     res.status(200).json(client)
 })
 
