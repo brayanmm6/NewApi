0f7929a620892e377b70defe09bbcef692e127d9
brayanmm6 <martinsbrayan514@gmail.com>
2025-01-27 21:02:52 -0300

    Melhorias nas rotas

--- src/routes/showTasks/index.js
+++ src/routes/showTasks/index.js
@@ -7,7 +7,7 @@ ShowTasksRoute.get("/show", async (req, res) => {
     try {
         res.setHeader("Access-Control-Allow-Origin", "*")
         const client = await db.show()
-        if(client.rows.length) res.status(200).json(client.rows)
+        res.status(200).json(client.rows)
     }catch (err) {
         res.status(404).send({message: "Houve um erro na sua requisição!"})
     }
