8207f0804e46f2dcdc9c1a48037516080fd1870e
brayanmm6 <martinsbrayan514@gmail.com>
2025-01-28 19:43:52 -0300

    Adicinando rotas

--- src/routes/showTasks/index.js
+++ src/routes/showTasks/index.js
@@ -7,7 +7,7 @@ showTasksRoute.get("/show", async (req, res) => {
     try {
         const client = await db.show()
         res.setHeader("Access-Control-Allow-Origin", "*")
-        res.status(200).json(client.rows)
+        res.status(200).json(client)
     }catch (err) {
         res.status(404).send({message: "Houve um erro na sua requisição!"})
     }
