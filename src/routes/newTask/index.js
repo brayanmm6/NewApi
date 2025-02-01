fbdca396587c3353eed5b918ae2cd6c6675fb918
brayanmm6 <martinsbrayan514@gmail.com>
2025-02-01 14:10:20 -0800

    Corrigindo erro no post

--- src/routes/newTask/index.js
+++ src/routes/newTask/index.js
@@ -4,6 +4,7 @@ const db = require("../../database")
 const newTaskRoute = exress.Router()
 
 newTaskRoute.post("/new-task", async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
     const client = await db.newTask(req.body.name, req.body.description, req.body.image)
     res.status(200).json(client)
 })
