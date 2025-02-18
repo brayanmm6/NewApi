917e390538d1fcc23d9ff2643d107cfdcd96204a
brayanmm6 <martinsbrayan514@gmail.com>
2025-02-17 21:00:45 -0300

    Corrigindo erro de cors na minha rota de new tasks

--- src/routes/newTask/index.js
+++ src/routes/newTask/index.js
@@ -3,14 +3,18 @@ const db = require("../../database")
 
 const newTaskRoute = exress.Router()
 
-newTaskRoute.post("/new-task", async (req, res) => {
+newTaskRoute.use(exress.json())
+
+newTaskRoute.post("/tasks/new", async (req, res) => {
     res.header({"Access-Control-Allow-Origin": "*",
-        'Access-Control-Allow-Credentials': 'true'
+        "accept": "application/json",
+        "content-type": "application/json; charset=utf-8"
     })
+    console.log(req.headers)
     console.log(req.body)
-    const client = await db.newTask(req.body.name, req.body.description, req.body.image)
+    //const client = await db.newTask(req.body.name, req.body.description, req.body.image)
     //res.status(200).json(client)
-    res.status(200).send({message: "Funfou"})
+    res.status(200).json(req.body)
 })
 
 module.exports = newTaskRoute
--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,17 @@
 const express = require("express")
-const db = require("../database")
 require('dotenv').config()
 const showTasksRoute = require("../routes/showTasks")
 const newTaskRoute = require("../routes/newTask")
 
 const app = express()
+
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 
 app.use(express.json())
 
@@ -13,16 +20,9 @@ app.use(showTasksRoute)
 app.use(newTaskRoute)
 
 app.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
     res.status(200).send("Home page")
 })
-
-// app.get("/show", async (req, res) => {
-//     const client = await db.show()
-//     // res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
-//     // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173")
-//     res.setHeader("Access-Control-Allow-Origin", "*")
-//     res.status(200).json(client)
-// })
 
 app.listen(process.env.PORT, (err) => {
     if(err) throw err
